f73c94f5cbb8e5ad667b7320e76a4e45fded9286
Tom Jakubowski <tom@crystae.net>
2012-03-18 21:55:52 -0700

    a few improvements
    - adds error handling for unknown artists
    - request to Drinkify includes a user-agent, so they can block or
      throttle the proxy if it annoys them
    - response includes the drinkify url for the artist

--- drinkify.rb
+++ drinkify.rb
@@ -11,14 +11,20 @@ class DrinkifyProxy < Sinatra::Base
     end
 
     def drinkify(raw_artist)
-      doc = Nokogiri::HTML(open(drinkify_url(raw_artist)))
-      artist = doc.css('.drinkListing p').first.content.match(/listen to (.*) alone/)[1]
+      begin
+        f = open(drinkify_url(raw_artist), "User-Agent" => "DrinkifyProxy")
+      rescue OpenURI::HTTPError
+        halt 404
+      end
 
+      doc = Nokogiri::HTML(f)
+      artist = doc.css('.drinkListing p').first.content.match(/listen to (.*) alone/)[1]
       {
         artist: artist,
         drink: (artist[0,3].downcase == "the" ? artist : "The #{artist}"),
         recipe: doc.css('.recipe > li').map { |i| i.content },
-        instructions: doc.css('.instructions').first.content.gsub(/\s+/, ' ').strip
+        instructions: doc.css('.instructions').first.content.gsub(/\s+/, ' ').strip,
+        url: drinkify_url(raw_artist)
       }
     end
   end
